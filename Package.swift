49764f0b319bfb717f4a78f40084904bad4a6be6
CypherPoet <CypherPoet@gmail.com>
2021-07-16 07:26:28 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,48 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MyLibrary",
+    platforms: [
+        .iOS(.v15),
+        .macOS(.v12),
+        .tvOS(.v15),
+        .watchOS(.v8),
+    ],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "MyLibrary",
+            targets: [
+                "MyLibrary",
+            ]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "MyLibrary",
+            dependencies: [
+            ],
+            path: "Sources/MyLibrary/"
+        ),
+        .testTarget(
+            name: "MyLibraryTests",
+            dependencies: [
+                "MyLibrary",
+            ],
+            path: "Tests/MyLibrary/",
+            exclude: [
+                "Data/README.md",
+                "Toolbox/README.md",
+            ]
+        ),
+    ]
+)
